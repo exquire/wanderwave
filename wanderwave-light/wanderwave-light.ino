638d59419c6b544cdbbf128d410416708265d2e7
Ian Neilson <mail@ianneilson.com>
2017-01-22 13:49:18 +1300

    Adjusted the light controller to global timing

--- wanderwave-light/wanderwave-light.ino
+++ wanderwave-light/wanderwave-light.ino
@@ -14,6 +14,11 @@ unsigned int flashCount = 20;
 // 1 second = 1,000 milliseconds
 const long onTime = 20; 
 
+// Automatic calculation of the offTime based on the above
+unsigned long totalOnTime = onTime * flashCount;
+unsigned long totalOffTime = 1000 - totalOnTime;
+unsigned long offTime = totalOffTime / flashCount;
+
 
 //------------------------------------------------------------------
 // Define our ledControl class
@@ -21,63 +26,29 @@ const long onTime = 20;
 
 class ledControl {
 
-    // Private vars only used by the class
-    private:
-    unsigned long ledLastSwitch;
-    
-    unsigned long OnTime;
-    unsigned long ledOffTime;
-    unsigned long flashCount;
-    unsigned long totalOnTime;
-    unsigned long totalOffTime;
-    unsigned long offTime;
-    unsigned long curTime;
+    // Class that controls the state of the LEDs
+    // Doesn't handle timing any more as this needs to be global
+
+    // Private vars to be only used here
+    unsigned int ledPin;
 
     // Public vars to be shared with the rest of the code
     public:
-    unsigned int ledPin;
     char note;
-    unsigned int ledState;
     unsigned int notePressed;
     
 
     // Constructor - creates an ledControl
-    ledControl(int _pin, char _note, long _ledOnTime, long _flashCount ) {
+    ledControl(int _pin, char _note ) {
       ledPin = _pin;
       pinMode(ledPin, OUTPUT);
       note = _note;
-      ledState = LOW;
       notePressed = 0;
-      ledLastSwitch = 0;
-      OnTime = _ledOnTime;
-      flashCount = _flashCount;
-      curTime = 0;
-
-      // Calculate the offTime
-      totalOnTime = onTime * flashCount;
-      totalOffTime = 1000 - totalOnTime;
-      offTime = totalOffTime / flashCount;
-
     }
 
-    // Function for switching on and off the light for this channel
-    void flashSwitch() {
-      curTime = millis();
-      
-      if (( ledState == LOW ) && ( curTime - ledLastSwitch >= offTime )) {
-        ledState = HIGH;
-        ledLastSwitch = curTime;
-        digitalWrite(ledPin, ledState);
-      } else if (( ledState == HIGH ) && ( curTime - ledLastSwitch >= onTime )) {
-        ledState = LOW;
-        ledLastSwitch = curTime;
-        digitalWrite(ledPin, ledState);
-      }
-    }
-
-    // Function to return the LED note
-    void getNote() {
-        return(note);
+    // Function to switch state as input
+    void switchLedState(unsigned int _state) {
+      digitalWrite(ledPin,_state);
     }
     
 }; // End definition of ledControl class
@@ -86,11 +57,11 @@ class ledControl {
 // Configure each LED channel here
 //------------------------------------------------------------------
 
-// input vars are (pin, note, onTime, flashCount)
-ledControl led1(10,'0',onTime, flashCount);
-ledControl led2(11,'2',onTime, flashCount);
-ledControl led3(12,'4',onTime, flashCount);
-ledControl led4(13,'5',onTime, flashCount);
+// input vars are (pin, note)
+ledControl led1(10,'0');
+ledControl led2(11,'2');
+ledControl led3(12,'4');
+ledControl led4(13,'5');
 
 //------------------------------------------------------------------
 // Leave below as is please!
@@ -101,39 +72,38 @@ void handleNoteOn(byte inChannel, byte inNote, byte inVelocity) {
     Serial.print("\n");
     if ( inNote == led1.note ) {
         led1.notePressed = 1; 
-        Serial.print("led1 key has been pressed\n");
     } else if ( inNote == led2.note ) { 
         led2.notePressed = 1; 
-        Serial.print("led2 key has been pressed\n");
     } else if ( inNote == led3.note ) { 
         led3.notePressed = 1; 
-        Serial.print("led3 key has been pressed\n");
     } else if ( inNote == led4.note ) { 
         led4.notePressed = 1; 
-        Serial.print("led4 key has been pressed\n");
     } 
 } // End handleNoteOn
 
 void handleNoteOff(byte inChannel, byte inNote, byte inVelocity) {
   if ( inNote == led1.note ) {
     led1.notePressed = 0;
-    led1.ledState = LOW;
-    digitalWrite(led1.ledPin, led1.ledState);
+    led1.switchLedState(LOW);
   } else if ( inNote == led2.note ) { 
     led2.notePressed = 0;
-    led2.ledState = LOW;
-    digitalWrite(led2.ledPin, led2.ledState);
+    led2.switchLedState(LOW);
   } else if ( inNote == led3.note ) { 
     led3.notePressed = 0;
-    led3.ledState = LOW;
-    digitalWrite(led3.ledPin, led3.ledState);
+    led3.switchLedState(LOW);
   } else if ( inNote == led4.note ) { 
     led4.notePressed = 0;
-    led4.ledState = LOW;
-    digitalWrite(led4.ledPin, led4.ledState);
+    led4.switchLedState(LOW);
   }  
 } // End handleNoteOff
 
+// Init the vars for global LED control
+unsigned int globalLedState = LOW; // Start with our state as lights off
+unsigned long ledLastSwitch = 0;
+unsigned long curTime = 0;
+
+
+// Setup function
 void setup() {
     MIDI.setHandleNoteOn(handleNoteOn);
     MIDI.setHandleNoteOff(handleNoteOff);
@@ -149,9 +119,17 @@ void setup() {
     Serial.print(led3.note);
     Serial.print("\n led4 note: ");
     Serial.print(led4.note);
+    Serial.print("\n");
+
+    // Make sure the lights are off when we start:
+    led1.switchLedState(LOW);
+    led2.switchLedState(LOW);
+    led3.switchLedState(LOW);
+    led4.switchLedState(LOW);
     
 } // End setup
 
+// Main loop function
 void loop() {
 
     //------------------------------------------------------------------
@@ -164,17 +142,39 @@ void loop() {
   // Begin LED control
   //------------------------------------------------------------------
 
-  if ( led1.notePressed == 1 ) {
-    led1.flashSwitch();
-  } 
-  if ( led2.notePressed == 1 ) {
-    led2.flashSwitch();
-  } 
-  if ( led3.notePressed == 1 ) {
-    led3.flashSwitch();
-  } 
-  if ( led4.notePressed == 1 ) {
-    led4.flashSwitch();
-  }
+    curTime = millis();      
+    if (( globalLedState == LOW ) && ( curTime - ledLastSwitch >= offTime )) {
+      globalLedState = HIGH;
+      ledLastSwitch = curTime;
+      if ( led1.notePressed = 1 ) {
+        led1.switchLedState(HIGH);
+      }
+      if ( led2.notePressed = 1 ) {
+        led2.switchLedState(HIGH);
+      }
+      if ( led3.notePressed = 1 ) {
+        led3.switchLedState(HIGH);
+      }
+      if ( led4.notePressed = 1 ) {
+        led4.switchLedState(HIGH);
+      }
+    }
+
+    if (( globalLedState == HIGH) && ( curTime - ledLastSwitch >= onTime )) {
+      globalLedState = LOW;
+      ledLastSwitch = curTime;
+      if ( led1.notePressed = 1 ) {
+        led1.switchLedState(LOW);
+      }
+      if ( led2.notePressed = 1 ) {
+        led2.switchLedState(LOW);
+      }
+      if ( led3.notePressed = 1 ) {
+        led3.switchLedState(LOW);
+      }
+      if ( led4.notePressed = 1 ) {
+        led4.switchLedState(LOW);
+      }
+    }
 }
 
